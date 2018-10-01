adfa70da5582e5dec7bbc4b06916ba2989f26cfe
BeNsAeI <behnam.saeidi@gmail.com>
2018-10-01 14:41:27 -0700

    user stories update

--- Image to depth map/Opencv example/example.py
+++ Image to depth map/Opencv example/example.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 
 imgL = cv2.imread('Left3.jpg',0)
 imgR = cv2.imread('Right3.jpg',0)
-#stereo = cv2.StereoBM_create(numDisparities=16, blockSize=5)
+#stereo = cv2.StereoBM_create(numDisparities=32, blockSize=13)
 stereo = cv2.StereoBM(cv2.STEREO_BM_BASIC_PRESET,ndisparities=16, SADWindowSize=15)
 disparity = stereo.compute(imgL,imgR)
 cv2.imwrite("Test3.jpg", disparity)
