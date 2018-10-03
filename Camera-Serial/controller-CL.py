a647e8986d046a9415bf906044d4dfff2a4f8995
BeNsAeI <behnam.saeidi@gmail.com>
2018-10-03 15:13:17 -0700

    python serial tool is command line capable

--- Camera-Serial/controller-CL.py
+++ Camera-Serial/controller-CL.py
@@ -1,12 +1,17 @@
+import time
+import sys
 import serial
 class SerialWrapper:
 	def __init__(self, device):
 		self.ser = serial.Serial(device, 9600)
-		print(self.ser.name)
+#		print(self.ser.name)
+		self.ser.flush()
 	def sendData(self, data):
+		self.ser.flush()
 		data += "\r\n"
 		self.ser.write(data.encode())
 	def readData(self):
+		self.ser.flush()
 		data = ""
 		line = []
 		for c in self.ser.read():
@@ -21,12 +26,15 @@ class SerialWrapper:
 			self.ser.close()
 
 def main():
+	outdata = "r"
+	if(len(sys.argv) > 1):
+		print("executing: "+sys.argv[1]+" -> "+str(len(sys.argv[1]))+" characters detected.")
+		outdata = str(sys.argv[1])
+	else:
+		print("Homing...")
+		outdata = "h"
 	mySerial = SerialWrapper('/dev/ttyUSB0')
-        #misc code here
-#	indata = mySerial.readData()
-#	print(indata)
-#	outdata = "rrrrruuuu"
-	outdata = "h"
 	mySerial.sendData(outdata)
+	time.sleep(0.25)
 	mySerial.close()
 main()
