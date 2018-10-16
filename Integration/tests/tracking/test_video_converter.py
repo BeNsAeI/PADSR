8c7526546e8f24226bb206dc861139894f266eff
Kamilla <kamilla.1201@gmail.com>
2018-10-16 14:26:16 -0700

    Added cmd-line arguments and camera movement direction detection.

--- Integration/tests/tracking/test_video_converter.py
+++ Integration/tests/tracking/test_video_converter.py
@@ -4,52 +4,144 @@ import numpy as np
 from tests.helpers.tester import Tester
 from tracking.video_converter import VideoConverter
 
-@mock.patch('tracking.video_converter.create_depth_map')
-@mock.patch('tracking.video_converter.cv2')
-@mock.patch('tracking.video_converter.os')
-def test_depthmaps_count(mocked_os, mocked_cv2, mocked_depth_map_func):
-    # set required mocks
-    mocked_depth_map_func.return_value = np.array([[3], [4]])
-    mocked_cv2.resize.side_effect = [np.array([1]), np.array([2]), np.array([3])]
+def mock_video_capture(mocked_cv2,total_frames_count):
+    '''
+    Helper method that mocks VideoCapture to return total_frames_count
+    number to frames
+    mocked_cv2: mocked cv2 module
+    total_frames_count: int - number of frames to return
+    '''
     mocked_capture = mocked_cv2.VideoCapture.return_value
     mocked_capture.isOpened.return_value = True
-    mocked_capture.read.side_effect = [(True, np.array([1])), (True, np.array([2])),
-            (True, np.array([3])), (False, None)]
-    mocked_writer = mocked_cv2.VideoWriter.return_value
+    # setting VideoCapture.read() function to return total_frames_count frames and then return None
+    mocked_capture.read.side_effect = [(True, np.array([i])) for i in range(total_frames_count)] + [(False, None)]
 
-    # call tested method
-    test_converter = VideoConverter()
-    test_converter.convert_video("any_input", "any_output")
-
-    assert mocked_writer.write.call_count == 2
 
 @mock.patch('tracking.video_converter.create_depth_map')
 @mock.patch('tracking.video_converter.cv2')
-@mock.patch('tracking.video_converter.os')
+@mock.patch('tracking.video_converter.os') # mock os to skip checking if files exist
+def test_depthmaps_step_1(mocked_os, mocked_cv2, mocked_depth_map_func):
+    '''
+    Verify that VideoConverter doesn't skip any frames when step=1
+    '''
+    total_frames_count = 15
+    # set required mocks
+
+    # mock VideoCapture object
+    mock_video_capture(mocked_cv2, total_frames_count)
+
+    # setting create_depth_map function to return a numpy array
+    mocked_depth_map_func.return_value = np.array([[0], [1]])
+
+    # setting cv2.resize() function to return a numpy array
+    mocked_cv2.resize.return_value = [np.array(0)]
+
+    # mock of VideoWriter object
+    mocked_writer = mocked_cv2.VideoWriter.return_value
+
+    test_converter = VideoConverter()
+    # call convert_video with step 1 and check that it saved total_frames_count-1 depth maps
+    test_converter.convert_video("any_input", "any_output", False, False, 1)
+    assert mocked_writer.write.call_count == total_frames_count-1
+
+@mock.patch('tracking.video_converter.create_depth_map')
+@mock.patch('tracking.video_converter.cv2')
+@mock.patch('tracking.video_converter.os') # mock os to skip checking if files exist
+def test_depthmaps_step_3(mocked_os, mocked_cv2, mocked_depth_map_func):
+    '''
+    Verify that VideoConverter processes every 3d frame when step=3
+    '''
+    total_frames_count = 15
+    # set required mocks
+
+    # mock VideoCapture object
+    mock_video_capture(mocked_cv2, total_frames_count)
+
+    # setting create_depth_map function to return a numpy array
+    mocked_depth_map_func.return_value = np.array([[0], [1]])
+
+    # setting cv2.resize() function to return a numpy array
+    mocked_cv2.resize.return_value = [np.array(0)]
+
+    # mock of VideoWriter object
+    mocked_writer = mocked_cv2.VideoWriter.return_value
+
+    test_converter = VideoConverter()
+    # call convert_video with step 3 and check that it saved (total_frames_count/3)-1 depth maps
+    test_converter.convert_video("any_input", "any_output", False, False, 3)
+    assert mocked_writer.write.call_count == (total_frames_count/3)-1
+
+@mock.patch('tracking.video_converter.os') # mock os to skip checking if files exist
+def test_depthmaps_step_0(mocked_os):
+    '''
+    Verify that VideoConverter raises error when step<=0
+    '''
+    test_converter = VideoConverter()
+    # call convert_video with step 0 and check that it returns ValueError
+    with pytest.raises(ValueError):
+        test_converter.convert_video("any_input", "any_output", False, False, 0)
+
+    # call convert_video with step -1 and check that it returns ValueError
+    with pytest.raises(ValueError):
+        test_converter.convert_video("any_input", "any_output", False, False, -1)
+
+@mock.patch('tracking.video_converter.create_depth_map')
+@mock.patch('tracking.video_converter.cv2')
+@mock.patch('tracking.video_converter.os') # mock os to skip checking if files exist
+def test_depthmaps_large_step(mocked_os, mocked_cv2, mocked_depth_map_func):
+    '''
+    Verify that VideoConverter doesn't process any frames when step > number of frames
+    '''
+    total_frames_count = 15
+    # set required mocks
+
+    # mock VideoCapture object
+    mock_video_capture(mocked_cv2, total_frames_count)
+
+    # setting create_depth_map function to return a numpy array
+    mocked_depth_map_func.return_value = np.array([[0], [1]])
+
+    # setting cv2.resize() function to return a numpy array
+    mocked_cv2.resize.return_value = [np.array(0)]
+
+    # mock of VideoWriter object
+    mocked_writer = mocked_cv2.VideoWriter.return_value
+
+    test_converter = VideoConverter()
+
+    # call convert_video with step > total_frames_count and check that it doesn't save anything
+    test_converter.convert_video("any_input", "any_output", False, False, total_frames_count+1)
+    assert mocked_writer.write.call_count == 0
+
+@mock.patch('tracking.video_converter.create_depth_map')
+@mock.patch('tracking.video_converter.cv2')
+@mock.patch('tracking.video_converter.os') # mock os to skip checking if files exist
 def test_one_frame(mocked_os, mocked_cv2, mocked_depth_map_func):
     # set required mocks
     mocked_cv2.resize.side_effect = [np.array([1])]
-    mocked_capture = mocked_cv2.VideoCapture.return_value
-    mocked_capture.isOpened.return_value = True
-    mocked_capture.read.side_effect = [(True, np.array([1])), (False, None)]
+    # set mocked VideoCapture to return 1 frame
+    mock_video_capture(mocked_cv2, 1)
+    # mock of VideoWriter object
     mocked_writer = mocked_cv2.VideoWriter.return_value
 
     # call tested method
     test_converter = VideoConverter()
-    test_converter.convert_video("any_input", "any_output")
+    test_converter.convert_video("any_input", "any_output", False, False, 1)
 
     assert mocked_cv2.resize.call_count == 1
     assert mocked_depth_map_func.called == False
     assert mocked_writer.write.called == False
 
-@mock.patch('tracking.video_converter.cv2')
 @mock.patch('tracking.video_converter.create_depth_map')
-def test_file_not_found(mocked_cv2, mocked_depth_map_func):
+def test_file_not_found(mocked_depth_map_func):
+    '''
+    Verify that function raises error if input does not exist
+    '''
     test_converter = VideoConverter()
     with pytest.raises(ValueError):
-        test_converter.convert_video("any_input", "any_output")
+        test_converter.convert_video("any_input", "any_output", False, False, 1)
 
-@pytest.mark.skip(reason="input video should be added")
+@pytest.mark.skip(reason="input video should be added to run this test")
 def test_performance():
     tester = Tester()
     test_converter = VideoConverter()
