3a2a9738ee01c3d110772e15a156d4f386dbd775
BeNsAeI <behnam.saeidi@gmail.com>
2018-10-16 11:29:37 -0700

    opengl python setup was added

---
+++ Lidar-OpenGL/OpenGL python/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt update
+sudo pip install hg+http://bitbucket.org/pygame/pygame
+sudo pip install pyyaml
+sudo pip install Pillow
+sudo pip install ftputil
+sudo pip install svn+https://svn.code.sf.net/p/python-xlib/code/trunk/
+sudo pip install PyOpenGL PyOpenGL_accelerate
+sudo apt install python-opengl
+sudo apt install python-pygame
+sudo easy_install PyOpenGL
+sudo easy_install -f . PyOpenGL
+sudo easy_install -U PyOpenGL
