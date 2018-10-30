22e1d0d20765e3572edd4a2dbca2197f97f4a659
BeNsAeI <behnam.saeidi@gmail.com>
2018-10-30 11:48:41 -0700

    added bluetooth functionlity

---
+++ Bluetooth/Bluetooth.py
@@ -0,0 +1,23 @@
+import bluetooth
+import sys
+bd_addr = "20:16:10:31:46:89"
+
+port = 1
+sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+sock.connect((bd_addr, port))
+print 'Connected'
+sock.settimeout(10.0)
+sock.send("w")
+print 'Sent data'
+
+while 1:
+	data = sock.recv(10)
+	print data
+	print "Send data or \'q\' to exit: "
+	tosend = raw_input()
+	if tosend != 'q':
+		sock.send(tosend)
+	else:
+		break
+
+sock.close()
