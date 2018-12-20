229e47d88bf58403a340511bab7fdd46c2e05f80
bensaei <behnam.saeidi@gmail.com>
2018-12-20 15:59:07 -0800

    Scheduler now supports agents

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class color_code:
 CC = color_code
 
 class Priority:
+	AGENT = 0
 	HIGH = 1
 	MEDIUM = 2
 	LOW = 3
@@ -84,9 +85,21 @@ def Task_re_schedule(args):
 	Task_print_w(["Rescheduling task: " + args[2], CC.Header, True])
 	schedule.append(args)
 
+def Task_test_schedule(args):
+	schedule.append([Task_impossible, [1], "TRY impossible task", Priority.TRY, False])
+	schedule.append([Task_print_w, ["test 1", CC.Warning], "MEDIUM priority print test", Priority.MEDIUM, False])
+	schedule.append([Task_print_w, ["test 2", CC.Warning], "LOW priority print test", Priority.LOW, False])
+	schedule.append([Task_print_w, ["test 3", CC.Warning], "IDLE priority print test", Priority.IDLE, False])
+	schedule.append([Task_print_w, ["test 4", CC.Warning], "TRY priority print test", Priority.TRY, False])
+
+Task_manager_Message = True
+
 def Task_manager(args):
 	global schedule
-	Task_print_w(["Running Task Manager ...", CC.Header, True])
+	global Task_manager_Message
+	if Task_manager_Message:
+		Task_print_w(["Running Task Manager ...", CC.Header, True])
+		Task_manager_Message = False
 	return 0
 
 # To-Do:
@@ -103,22 +116,23 @@ def Task_initialize(args):
 	schedule = []
 	Task_print_w(["Initializing ...", CC.Header, True])
 	#schedule.append([<TASK>, [<ARGS>], "<TASK NAME>", Priority.<PRIORITY>, <QUITE>])
-	schedule.append([Task_impossible, [1], "TRY impossible task", Priority.TRY, False])
-	schedule.append([Task_print_w, ["test 1", CC.Warning], "MEDIUM priority print test", Priority.MEDIUM, False])
-	schedule.append([Task_print_w, ["test 2", CC.Warning], "LOW priority print test", Priority.LOW, False])
-	schedule.append([Task_print_w, ["test 3", CC.Warning], "IDLE priority print test", Priority.IDLE, False])
-	schedule.append([Task_print_w, ["test 4", CC.Warning], "TRY priority print test", Priority.TRY, False])
-	schedule.append([Task_manager, [], "Lunching Manager", Priority.HIGH, False])
+	#Task_test_schedule([])
+	schedule.append([Task_manager, [], "Lunching Manager", Priority.AGENT, False])
 	Task_print_w(["Initial tasks were scheduled.", CC.Pass, True])
 
 def Task_lunch(task, args, task_name, priority = Priority.IDLE, quite = True):
 	try:
 		task(args)
 		str_priority = ""
-		if not quite:
+		if not quite and priority != Priority.AGENT:
 			Task_print_w(["Task[-]: \"" + task_name + "\" was successful.", CC.Successful])
 	except:
-		if priority == Priority.HIGH:
+		if priority == Priority.AGENT:
+			#agents are never quite on failure!
+			Task_print_w(["Task[HIGH]: \"" + task_name + "\" Failed.", CC.Failed])
+			exit(1)
+
+		elif priority == Priority.HIGH:
 			if not quite:
 				Task_print_w(["Task[HIGH]: \"" + task_name + "\" Failed.", CC.Failed])
 			Task_lunch(Task_diagnostics, [0] , "Lunching Diagnostics",Priority.HIGH, quite= quite)
@@ -143,8 +157,15 @@ def main():
 	global schedule
 	Task_lunch(Task_initialize,[], "Initializing", Priority.HIGH, quite = False)
 	while len(schedule) != 0:
-		
-		Task_print_w(["Executing HIGH priority scheduled items", CC.Normal])
+		#Task_print_w(["Executing AGENT priority scheduled items", CC.Normal])
+		i = 0
+		while (i < len(schedule)) and (i > -1):
+			if schedule[i][3] == Priority.AGENT:
+				Task_lunch(schedule[i][0], schedule[i][1], schedule[i][2], schedule[i][3], quite=schedule[i][4])
+				#Agents are not unscheduled
+			i += 1
+
+		#Task_print_w(["Executing HIGH priority scheduled items", CC.Normal])
 		i = 0
 		while (i < len(schedule)) and (i > -1):
 			if schedule[i][3] == Priority.HIGH:
@@ -152,7 +173,7 @@ def main():
 				schedule.remove(schedule[i])
 			i += 1
 
-		Task_print_w(["Executing MEDIUM priority scheduled items", CC.Normal])
+		#Task_print_w(["Executing MEDIUM priority scheduled items", CC.Normal])
 		i = 0
 		while (i < len(schedule)) and (i > -1):
 			if schedule[i][3] == Priority.MEDIUM:
@@ -161,7 +182,7 @@ def main():
 			i += 1
 
 
-		Task_print_w(["Executing LOW priority scheduled items", CC.Normal])
+		#Task_print_w(["Executing LOW priority scheduled items", CC.Normal])
 		i = 0
 		while (i < len(schedule)) and (i > -1):
 			if schedule[i][3] == Priority.LOW:
@@ -169,7 +190,7 @@ def main():
 				schedule.remove(schedule[i])
 			i += 1
 
-		Task_print_w(["Executing IDLE priority scheduled items", CC.Normal])
+		#Task_print_w(["Executing IDLE priority scheduled items", CC.Normal])
 		i = 0
 		while (i < len(schedule)) and (i > -1):
 			if schedule[i][3] == Priority.IDLE:
@@ -177,7 +198,7 @@ def main():
 				schedule.remove(schedule[i])
 			i += 1
 
-		Task_print_w(["Executing TRY priority scheduled items", CC.Normal])
+		#Task_print_w(["Executing TRY priority scheduled items", CC.Normal])
 		i = 0
 		while (i < len(schedule)) and (i > -1):
 			if schedule[i][3] == Priority.TRY:
